ea5d90d7247224a00205d7a1c29c1db354e782dc
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-07 10:33:23 +0545

    add login and logout message

--- index.php
+++ index.php
@@ -3,8 +3,15 @@ require 'includes/header.php';
 ?>
 
 <main class="banner-box">
-    <p>You are logged out</p>
-    <p>You are logged in!</p>
+    <?php
+    if (isset($_SESSION['userId'])) {
+        echo "<p>You are logged in!</p>";
+    } else {
+        echo "<p>You are logged out</p>";
+    }
+    ?>
+
+
 </main>
 
 <?php
