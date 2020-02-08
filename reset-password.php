db20b23ed1adcb89cf72888e5e54888dcd601854
Aashish Bogati <aashishbogati@users-MacBook-Air.local>
2020-02-08 11:38:38 +0545

    Initial commit

---
+++ reset-password.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php';
+
+?>
+
+<main class="box">
+    <h1 class="w-700">Forgot your password</h1>
+    <p class="secondary-text">An e-mail will be send you with instructions on how to reset your password</p>
+    <form action="" method="post">
+        <input type="text" name="mail" id="" placeholder="Enter your e-mail address">
+        <button type="submit" class='btn text-upper'>Receive new password by mail</button>
+    </form>
+</main>
+
+<?php include 'includes/footer.php' ?>
